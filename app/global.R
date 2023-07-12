323710ca23873000fda13a9d955b99d772f34596
Alexis Roldan <argos2416@gmail.com>
2023-07-11 18:41:30 -0700

    UI Updated 11Jul23

--- app/global.R
+++ app/global.R
@@ -8,11 +8,15 @@ library(glue)
 # source("utils/functions.R")
 my_theme <- bs_theme(
   bootswatch = "darkly",
-  base_font = font_google("Righteous"))
+  heading_font = font_google("Lobster"),
+  base_font = font_collection(font_google("Roboto Slab"), font_google("Merriweather")),
+  # base_font = c("Grandstander", "sans-serif"),
+  code_font = font_google("Inconsolata")
+  )
 
 chat_response <- function(
     user_message, history = NULL,
-    system_prompt = c("general", "r_code", "python_code"),
+    system_prompt = c("general", "r_code", "python_code", "sql_code"),
     api_key = Sys.getenv("OPENAI_API_SHINY"),
     model_selected = model) {
 
@@ -28,7 +32,7 @@ chat_response <- function(
             request(chat_url) |>
             req_auth_bearer_token(token = api_key) |>
             req_headers("Content-Type" = "application/json") |>
-            req_user_agent("Alexis Roldan @roldanalex | Personal Bot") |>
+            req_user_agent("Alexis Roldan @roldanalex | Personal Chatbot") |>
             req_body_json(body) |>
             req_retry(max_tries = 4) |>
             req_throttle(rate = 15) |>
@@ -39,14 +43,16 @@ chat_response <- function(
         openai_chat_response$choices$message$content
 }
 
-get_system_prompt <- function(system = c("general", "r_code", "python_code")) {
+get_system_prompt <- function(
+    system = c("general", "r_code", "python_code", "sql_code")) {
 
     rlang::arg_match(system)
     instructions <- switch(
         system,
         "general" = "You are an useful and resourceful assistant.",
-        "r_code" = "You are a resourceful and efficient chat bot that answers questions for an R programmer.", # nolint: line_length_linter.
-        "python_code" = "You are an efficient and resourceful chat bot that answers questions for an Python programmer.") # nolint: line_length_linter.
+        "r_code" = "You are a resourceful and efficient chat bot that answers questions for a R programmer.", # nolint: line_length_linter.
+        "python_code" = "You are an efficient and resourceful chat bot that answers questions for a Python programmer.", # nolint: line_length_linter.
+        "sql_code" = "You are an efficient and resourceful chat bot that answers questions for a SQL developer") # nolint: line_length_linter.
 
     list(list(role = "system", content = instructions))
 
@@ -55,7 +61,9 @@ get_system_prompt <- function(system = c("general", "r_code", "python_code")) {
 prepare_prompt <- function(user_message, system_prompt, history) {
 
     user_prompt <-  list(list(role = "user", content = user_message))
-    c(system_prompt, history, user_prompt) |> purrr::compact()
+    
+    c(system_prompt, history, user_prompt) |>
+      purrr::compact()
 
 }
 
--- app/ui.R
+++ app/ui.R
@@ -1,45 +1,79 @@
 page_sidebar(
-  title = "Personal AI Chatbot (Powered by OpenAI GTP models)",
-  subtitle = "Created by Alexis Roldan",
+  title = " Personal AI Chatbot (Powered by OpenAI)",
   theme = my_theme,
   sidebar = sidebar(
     open = "open",
     radioButtons(
       "current_theme", "App Theme:",
-      c("Light" = "cerulean", "Dark" = "darkly"),
+      c("Light" = "zephyr", "Dark" = "darkly"),
       selected = "darkly",
       inline = TRUE
     ),
     selectInput(
-      "model", "Select your GTP model",
-      choices = c("gpt-3.5-turbo", "gpt-4"),
+      "model", "Select your GTP model:",
+      choices = c("gpt-3.5-turbo"),
       selected = "gpt-3.5-turbo"),
     selectInput(
-      "task", "Select your prompt type",
+      "task", "Select your prompt type:",
       choices = list(
         "General" = "general",
         "R Code" = "r_code",
-        "Python Code" = "python_code"),
+        "Python Code" = "python_code",
+        "SQL Code" = "sql_code"),
       selected = "general"),
   ),
   textAreaInput(
     "user_prompt", NULL,
-    width = "600px",
+    width = "800px",
     height = "200px"),
-  actionBttn(
-    "send_prompt",
-    label = NULL,
-    color = "danger",
-    size = "md",
-    style = "jelly",
-    icon = icon("paper-plane"),
-    width = "80px",
-    block = FALSE),
+  column(
+    1,
+    shinyBS::bsTooltip(
+      id = "send_prompt", placement = 'top',
+      title = "Click here to send prompt to chatbot",
+      trigger = "hover"),
+    actionBttn(
+      "send_prompt",
+      label = NULL,
+      color = "danger",
+      size = "md",
+      style = "jelly",
+      icon = icon("paper-plane"),
+      # width = "80px",
+      block = TRUE)
+  ),
   # actionButton(
   #   "chat", NULL, 
   #   icon = icon("paper-plane"),
   #   width = "80px",
   #   class = "m-2 btn-success"
   # ),
-  uiOutput("chat_history")
+  uiOutput("chat_history"),
+  div(style = "margin-bottom: 30px;"), # this adds breathing space between content and footer
+  tags$footer(
+    fluidRow(
+      column(4, "© Alexis Roldan - 2023"),
+      column(4, "Personal Chatbot v1.0.3"),
+      column(
+        4,
+        tags$a(
+          href = "mailto:alexis.m.roldan.ds@gmail.com",
+          tags$b("Email me"),
+          class = "externallink",
+          style = "color: white; text-decoration: none")
+        ),
+      style = "
+        position:fixed;
+        text-align:center;
+        left: 0;
+        bottom:0;
+        width:100%;
+        z-index:1000;  
+        height:30px; /* Height of the footer */
+        color: white;
+        padding: 3px;
+        font-weight: bold;
+        background-color: #333333"
+    )
+  )
 )
