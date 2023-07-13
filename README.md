25ff4487098b9ce580cfa01a6dd94a2cb87c1f73
Alexis Roldan <argos2416@gmail.com>
2023-07-13 00:22:42 -0700

    README updated 13Jul23

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
-# Personal Ai Chatbot
+# Personal AI Chatbot
 
-**IMPORTANT:** Before cloning this repo and running the logic, please make sure to first register for an *OpenAI API Key*.
+**IMPORTANT:** 
+**Before cloning this repo and running the logic, please make sure to first register for an *OpenAI API Key*.**
 
 # Overview
 Interactive chatbot app developed using R/Shiny and powered by OpenAI's gtp-3.5-turbo model. This personal chatbot can store prompt history to increase response efficiency.
 
 ## Preview - Full Window
 <!-- <img src="figs/GTP_Assistant_Shiny_App.png" alt="GTP Example Full"> -->
+
+Please see below for screenshot of app web version.
 ![Personal AI Chatbot - Web Version](figs/Personal-AI-Chatbot.png)
 
 ## Preview - Mobile Version
+
+Screenshot below show app mobile version.
 <img width="400px" src="figs/Personal-Ai-Chatbot-Mobil.jpeg" alt="Personal AI Chatbot - Mobile Version">
 <!-- ![Personal AI Chatbot - Mobile Version](figs/Personal-Ai-Chatbot-Mobil.jpeg) -->
 
--- app/utils/functions.R
+++ app/utils/functions.R
@@ -1,22 +1,31 @@
-chat <- function(
+my_theme <- bs_theme(
+  bootswatch = "darkly",
+  heading_font = font_google("Lobster"),
+  base_font = font_collection(
+    font_google("Roboto Slab"), font_google("Merriweather")
+    ),
+  code_font = font_google("Inconsolata")
+)
+
+chat_response <- function(
     user_message, history = NULL,
-    system_prompt = c("general", "code"),
-    api_key = Sys.getenv("OPENAI_API")) {
+    system_prompt = c("general", "r_code", "python_code", "sql_code"),
+    api_key = Sys.getenv("OPENAI_API_SHINY"),
+    model_selected = model) {
 
         system <- get_system_prompt(system_prompt)
         prompt <- prepare_prompt(user_message, system, history)
-        base_url <- "https://api.openai.com/v1"
+        chat_url <- Sys.getenv("OPENAI_COMPLETION_URL")
         body <- list(
-            model = "gpt-3.5-turbo",
+            model = model_selected,
             messages = prompt)
 
         req <-
             resp <-
-            request(base_url) |>
-            req_url_path_append("chat/completions") |>
+            request(chat_url) |>
             req_auth_bearer_token(token = api_key) |>
             req_headers("Content-Type" = "application/json") |>
-            req_user_agent("Alexis Roldan @roldanalex | Personal Bot") |>
+            req_user_agent("Alexis Roldan @roldanalex | Personal Chatbot") |>
             req_body_json(body) |>
             req_retry(max_tries = 4) |>
             req_throttle(rate = 15) |>
@@ -25,15 +34,18 @@ chat <- function(
         openai_chat_response <- resp |> resp_body_json(simplifyVector = TRUE)
 
         openai_chat_response$choices$message$content
-}
+    }
 
-get_system_prompt <- function(system = c("general", "code")) {
+get_system_prompt <- function(
+    system = c("general", "r_code", "python_code", "sql_code")) {
 
     rlang::arg_match(system)
     instructions <- switch(
         system,
-        "general" = "You are a helpful assistant.",
-        "code" = "You are a helpful chat bot that answers questions for an R programmer working in the RStudio IDE.") # nolint: line_length_linter.
+        "general" = "You are an useful and resourceful assistant.",
+        "r_code" = "You are a resourceful and efficient chat bot that answers questions for a R programmer.", # nolint: line_length_linter.
+        "python_code" = "You are an efficient and resourceful chat bot that answers questions for a Python programmer.", # nolint: line_length_linter.
+        "sql_code" = "You are an efficient and resourceful chat bot that answers questions for a SQL developer") # nolint: line_length_linter.
 
     list(list(role = "system", content = instructions))
 
@@ -42,7 +54,9 @@ get_system_prompt <- function(system = c("general", "code")) {
 prepare_prompt <- function(user_message, system_prompt, history) {
 
     user_prompt <-  list(list(role = "user", content = user_message))
-    c(system_prompt, history, user_prompt) |> purrr::compact()
+
+    c(system_prompt, history, user_prompt) |>
+      purrr::compact()
 
 }
 
